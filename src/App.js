0bcc65a7f675dba104ec55c831e7ca987bfbb423
Tolotra Raharison <tolotrq.raharison@gmail.com>
2018-11-22 01:07:08 +0300

    prettify code

--- src/App.js
+++ src/App.js
@@ -1,55 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Layout from 'antd/lib/layout/layout';
-import Form from 'antd/lib/form/Form'
-import Input from 'antd/lib/input/Input'
-import Button from 'antd/lib/button/button'
-import Icon from 'antd/lib/icon'
-import FormItem from 'antd/lib/form/FormItem'
-import DatePicker from 'antd/lib/date-picker';  // for js
+import Form from 'antd/lib/form/Form';
+import Input from 'antd/lib/input/Input';
+import Button from 'antd/lib/button/button';
+import Icon from 'antd/lib/icon';
+import FormItem from 'antd/lib/form/FormItem';
+import DatePicker from 'antd/lib/date-picker';
 import 'antd/lib/date-picker/style/css';
 
 import SiderLayout from './components/layout/SiderLayout';
 import HeaderLayout from './components/layout/HeaderLayout';
 import ContentLayout from './components/layout/ContentLayout';
 
-class App extends Component {
-  render () {
-    return (
+function App() {
+  return (
+    <Layout>
+      <HeaderLayout />
       <Layout>
-        <HeaderLayout></HeaderLayout>
-        <Layout>
-          <SiderLayout></SiderLayout>
-          <ContentLayout>
-            <Form layout="vertical">
-              <FormItem>
-                <Input
-                  size="large"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Username" />
-              </FormItem>
-              <FormItem>
-                <Input
-                  size="large"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Username" />
-              </FormItem>
-              <FormItem>
-                <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-              </FormItem>
-              <FormItem>
-                <Button
-                  size="large"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}>
-                  Submit
-                </Button>
-              </FormItem>
-            </Form>
-          </ContentLayout>
-        </Layout>
+        <SiderLayout />
+        <ContentLayout>
+          <Form layout="vertical">
+            <FormItem>
+              <Input
+                size="large"
+                prefix={
+                  <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder="Username"
+              />
+            </FormItem>
+            <FormItem>
+              <Input
+                size="large"
+                prefix={
+                  <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder="Username"
+              />
+            </FormItem>
+            <FormItem>
+              <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+            </FormItem>
+            <FormItem>
+              <Button
+                size="large"
+                prefix={
+                  <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+              >
+                Submit
+              </Button>
+            </FormItem>
+          </Form>
+        </ContentLayout>
       </Layout>
-    );
-  }
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/layout/ContentLayout.js
+++ src/components/layout/ContentLayout.js
@@ -3,14 +3,23 @@ import Layout from 'antd/lib/layout/layout';
 
 const { Content } = Layout;
 
-function ContentLayout ({ children }) {
+function ContentLayout({ children }) {
   return (
-    <Layout style={{ padding: '24px', overflow: 'auto', height: 'calc(100vh - 64px)', background: '#fff' }}>
-      <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 1280 }}>
+    <Layout
+      style={{
+        padding: '24px',
+        overflow: 'auto',
+        height: 'calc(100vh - 64px)',
+        background: '#fff'
+      }}
+    >
+      <Content
+        style={{ background: '#fff', padding: 24, margin: 0, minHeight: 1280 }}
+      >
         {children}
       </Content>
     </Layout>
   );
 }
 
-export default ContentLayout
+export default ContentLayout;
